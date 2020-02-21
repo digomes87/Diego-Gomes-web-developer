7c8112806d74b57611679dd69a6e58519b480d2b
Diego G <diego.gomes87@gmail.com>
2020-02-21 08:51:27 -0300

    little update in informations

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -60,7 +60,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love woking with amazing peoples.", "also teach programming to people.", "solve problems."
+				"I am a Full-Stack Web Developer,", "I love working with amazing people,", "I teach programming,", "I solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
